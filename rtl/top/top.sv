e2144d188767f36cbcc51102fa75d8367660dd52
ben <cowleyben23@gmail.com>
2026-03-26 15:06:44 +0800

    fix errors in top.sv

--- rtl/top/top.sv
+++ rtl/top/top.sv
@@ -1,23 +1,29 @@
 module top (
     input logic clk,
     input logic rst_n,
+    input logic start,
     output logic [31:0] x,
     output logic [31:0] y,
     output logic valid,
-    output logic frameEnd,
-    output logic frameStart
-
-
+    output logic frame_end,
+    output logic done,
+    input logic downstream_ready,
+    output logic ready,
+    output logic frame_start
 );
   // Instantiate pixel
   renderer pTest (
       .clk(clk),
       .rst_n(rst_n),
+      .start(start),
       .x(x),
       .y(y),
-      .frameEnd(frameEnd),
-      .frameStart(frameStart),
-      .valid(valid)
+      .frame_end(frame_end),
+      .frame_start(frame_start),
+      .done(done),
+      .valid(valid),
+      .downstream_ready(downstream_ready),
+      .ready(ready)
   );
 
 
